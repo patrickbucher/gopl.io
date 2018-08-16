27bf7abfbb9241f09957ac002975984c95e3b770
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-08-16 20:58:58 +0200

    exercise 3.6

--- ch03/ex03_06/main.go
+++ ch03/ex03_06/main.go
@@ -25,7 +25,37 @@ func main() {
 			img.Set(px, py, mandelbrot(z))
 		}
 	}
+	supersample(img)
 	png.Encode(os.Stdout, img) // NOTE: Ignoring errors
+}
+
+func supersample(orig *image.RGBA) {
+	const subpixels = 2 // 2 in both dimensions: 4 subpixels
+	w, h := orig.Bounds().Dx(), orig.Bounds().Dy()
+	wc, hc := w*subpixels, h*subpixels
+	copy := image.NewRGBA(image.Rect(0, 0, wc, hc))
+	for xc := 0; xc < wc; xc++ {
+		for yc := 0; yc < hc; yc++ {
+			copy.Set(xc, yc, orig.At(xc/subpixels, yc/subpixels))
+		}
+	}
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			var rs, gs, bs byte
+			for xc := x * subpixels; xc < x*subpixels+subpixels; xc++ {
+				for yc := y * subpixels; yc < y*subpixels+subpixels; yc++ {
+					r, g, b, _ := copy.At(xc, yc).RGBA()
+					rs += byte(r)
+					gs += byte(g)
+					bs += byte(b)
+				}
+			}
+			rs /= subpixels * subpixels
+			gs /= subpixels * subpixels
+			bs /= subpixels * subpixels
+			orig.Set(x, y, color.RGBA{R: rs, G: gs, B: bs, A: 0xff})
+		}
+	}
 }
 
 func mandelbrot(z complex128) color.Color {
