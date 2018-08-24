98b92c80dd4dfbcf02f1a151b633b5b17903cf7d
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-08-24 06:29:46 +0200

    chapter 4.3

---
+++ ch04/rev/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(a[:])
+	fmt.Println(a) // "[5 4 3 2 1 0]"
+
+	s := []int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions
+	reverse(s[:2])
+	reverse(s[2:])
+	reverse(s)
+	fmt.Println(s) // "[2 3 4 5 0 1]"
+}
+
+// reverse reverses a slice of ints in place.
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
