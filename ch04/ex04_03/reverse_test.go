ae5c9df8534d3d5fc8160bc6d18f5b8f03a1ad12
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-08-24 06:47:37 +0200

    exercise 4.3

---
+++ ch04/ex04_03/reverse_test.go
@@ -0,0 +1,31 @@
+package ex04_10
+
+import "testing"
+
+type test struct {
+	Input    [3]int
+	Expected [3]int
+}
+
+var tests = []test{
+	{Input: [...]int{1, 2, 3}, Expected: [...]int{3, 2, 1}},
+}
+
+func TestReverse(t *testing.T) {
+	for i := range tests {
+		var input *[3]int
+		input = &tests[i].Input
+		if Reverse(input); !equal(input, &tests[i].Expected) {
+			t.Errorf("expected %v, got %v\n", tests[i].Expected, tests[i].Input)
+		}
+	}
+}
+
+func equal(x, y *[3]int) bool {
+	for i := 0; i < 3; i++ {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
