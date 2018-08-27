5e19d3fcbf52883e95bfe9b0785da2bd672de99e
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-08-27 23:31:03 +0200

    chapter 4.4

---
+++ ch04/treesort/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	const size = 10
+	values := make([]int, 0)
+	for i := 0; i < size; i++ {
+		values = append(values, rand.Intn(size))
+	}
+	fmt.Println(values)
+	Sort(values)
+	fmt.Println(values)
+}
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues appends the elements of t to values in order
+// and returns the resulting slice
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}.
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
