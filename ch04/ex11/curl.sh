6a0f00de454b86276fed0c6f6a3dad2086b249ac
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-08-29 23:07:29 +0200

    script for receiving GitHub API URLs

--- ch04/ex11/curl.sh
+++ ch04/ex11/curl.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-curl https://api.github.com/?access_token=`cat .github_token`
+#curl https://api.github.com/?access_token=`cat .github_token`
+curl -H "Authorization: token `cat .github_token`" https://api.github.com
