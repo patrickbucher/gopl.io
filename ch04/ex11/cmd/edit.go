a187015f94e5715a7a6826e92b3569402e5764f4
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-08-30 07:06:37 +0200

    Exercise 4.11: improved editor code

--- ch04/ex11/cmd/edit.go
+++ ch04/ex11/cmd/edit.go
@@ -9,23 +9,19 @@ import (
 )
 
 func main() {
-	var file *os.File
-	var data []byte
-	if f, err := ioutil.TempFile("", "ex4.11"); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	} else {
-		file = f
-	}
-	if err := ex11.Edit(file.Name()); err != nil {
+	file, err := ioutil.TempFile("", "ex4.11")
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	if d, err := ioutil.ReadFile(file.Name()); err != nil {
+	if err = ex11.Edit(file.Name()); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	} else {
-		data = d
+	}
+	data, err := ioutil.ReadFile(file.Name())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	fmt.Println(string(data))
 }
--- ch04/ex11/edit.go
+++ ch04/ex11/edit.go
@@ -9,11 +9,9 @@ import (
 const editorVariable = "EDITOR"
 
 func Edit(file string) error {
-	var editor string
-	if val, ok := os.LookupEnv(editorVariable); !ok {
+	editor, ok := os.LookupEnv(editorVariable)
+	if !ok {
 		return fmt.Errorf("environment variable %s must be set", editorVariable)
-	} else {
-		editor = val
 	}
 	cmd := exec.Command(editor, file)
 	cmd.Stdin = os.Stdin
