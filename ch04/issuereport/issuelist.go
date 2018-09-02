020d7e45187d5cba2a4ef2c095f7d8ce8abbdd9f
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-09-02 09:28:54 +0200

    Chapter 4.6: Templates

---
+++ ch04/issuereport/issuelist.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+
+	"gopl.io/ch04/github"
+)
+
+const temp = `{{.TotalCount}} issues:
+{{range .Items}}-------------------------------------------------------
+Number: {{.Number}}
+User:	{{.User.Login}}
+Title:	{{.Title | printf "%.64s"}}
+Age:	{{.CreatedAt | daysAgo}} days
+{{end}}`
+
+var report = template.Must(template.New("issuelist").
+	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+	Parse(temp))
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
