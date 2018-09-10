7ca6db79832b73a6cdccd39f631b5975a32143a6
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-09-10 21:38:41 +0200

    Chapter 5.6

---
+++ ch05/squares/sqares.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// squares returns a function that returns
+// the next square number each time it is called.
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
+func main() {
+	f := squares()
+	fmt.Println(f()) // "1"
+	fmt.Println(f()) // "4"
+	fmt.Println(f()) // "9"
+	fmt.Println(f()) // "16"
+}
