90a26485b8d80e313c89a7fc5d5702230ce2a978
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-09-18 06:51:14 +0200

    Chapter 5.8

---
+++ ch05/trace/trace.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	bigSlowOperation()
+}
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")() // don't forget the extra parentheses
+	// ...lots of work...
+	time.Sleep(10 * time.Second) // simulate slow operation by sleeping
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() { log.Printf("exit %s (%s)", msg, time.Since(start)) }
+}
