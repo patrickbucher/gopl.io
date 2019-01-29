3b4845bfeeba8fa768262815c5a87895eec067c2
patrickbucher <patrick.bucher@stud.hslu.ch>
2019-01-29 22:15:18 +0100

    Exercise 7.1: simplified test

--- ch07/ex01/counter_test.go
+++ ch07/ex01/counter_test.go
@@ -2,47 +2,32 @@ package counter
 
 import "testing"
 
-var wordTests = []struct {
+var tests = []struct {
 	input string
 	words int
-}{
-	{"", 0},
-	{"yes", 1},
-	{"yes no", 2},
-	{"what is it", 3},
-	{"this is a test", 4},
-	{"this is yet another test", 5},
-}
-
-func TestWordCounter(t *testing.T) {
-	for i := range wordTests {
-		var w WordCounter
-		input, expected := wordTests[i].input, wordTests[i].words
-		w.Write([]byte(input))
-		if int(w) != expected {
-			t.Errorf("word count for %q should be %v but was %v", input, expected, w)
-		}
-	}
-}
-
-var lineTests = []struct {
-	input string
 	lines int
 }{
-	{"", 0},
-	{"this", 1},
-	{"this\nis", 2},
-	{"this\nis", 2},
-	{"this\nis\na\ntest", 4},
+	{"", 0, 0},
+	{"this", 1, 1},
+	{"this is", 2, 1},
+	{"this is\na", 3, 2},
+	{"this is\na test", 4, 2},
+	{"this is\na stupid test", 5, 2},
+	{"this is\nyet another\nstupid test", 6, 3},
 }
 
-func TestLineCounter(t *testing.T) {
-	for i := range lineTests {
-		var l LineCounter
-		input, expected := lineTests[i].input, lineTests[i].lines
-		l.Write([]byte(input))
-		if int(l) != expected {
-			t.Errorf("line count for %q should be %v but was %v", input, expected, l)
+func TestCounter(t *testing.T) {
+	for i := range tests {
+		var wc WordCounter
+		var lc LineCounter
+		input, words, lines := tests[i].input, tests[i].words, tests[i].lines
+		wc.Write([]byte(input))
+		if int(wc) != words {
+			t.Errorf("word counter for %q should be %v but was %v", input, words, wc)
+		}
+		lc.Write([]byte(input))
+		if int(lc) != lines {
+			t.Errorf("line counter for %q should be %v but was %v", input, lines, lc)
 		}
 	}
 }
