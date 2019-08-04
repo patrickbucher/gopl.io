715761ec6d1595ae3047eb7b57e03da2b0ad6b92
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-08-04 10:05:34 +0200

    Exercise 8.4

---
+++ ch08/ex04/echo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn) // handle one connection at a time
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	var wg sync.WaitGroup
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		wg.Add(1)
+		go echo(c, input.Text(), 1*time.Second, &wg)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	wg.Wait()
+	if c, ok := c.(*net.TCPConn); ok {
+		c.CloseWrite()
+	}
+}
