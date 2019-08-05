ba7a323c09db349a51a645a4e3c61e4c6f491314
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-08-05 07:02:04 +0200

    Exercise 8.5: proper benchmark using N iterations; added test cases

--- ch08/ex05/arrsum_test.go
+++ ch08/ex05/arrsum_test.go
@@ -3,32 +3,73 @@ package arrsum
 import "testing"
 
 var (
-	n     = int(1e8) * 2
+	n     = int(1e6)
 	min   = 1
 	max   = 999
 	input = RandomNumbers(n, min, max)
+	want  = testSum(input)
 )
 
+func testSum(numbers []int) int {
+	sum := 0
+	for _, v := range numbers {
+		sum += v
+	}
+	return sum
+}
+
+func TestSerial(t *testing.T) {
+	got := Sum(input)
+	if got != want {
+		t.Errorf("TestSerial: expected %d, got %d", want, got)
+	}
+}
+
+func TestConcurrently(t *testing.T) {
+	got := SumConcurrently(input, 4)
+	if got != want {
+		t.Errorf("TestConcurrentlz: expected %d, got %d", want, got)
+	}
+}
+
 func BenchmarkSerial(b *testing.B) {
-	Sum(input)
+	for i := 0; i < b.N; i++ {
+		Sum(input)
+	}
 }
 
 func BenchmarkOneGoroutine(b *testing.B) {
-	SumConcurrently(input, 1)
+	for i := 0; i < b.N; i++ {
+		SumConcurrently(input, 1)
+	}
 }
 
 func BenchmarkTwoGoroutines(b *testing.B) {
-	SumConcurrently(input, 2)
+	for i := 0; i < b.N; i++ {
+		SumConcurrently(input, 2)
+	}
 }
 
 func BenchmarkThreeGoroutines(b *testing.B) {
-	SumConcurrently(input, 3)
+	for i := 0; i < b.N; i++ {
+		SumConcurrently(input, 3)
+	}
 }
 
 func BenchmarkFourGoroutines(b *testing.B) {
-	SumConcurrently(input, 4)
+	for i := 0; i < b.N; i++ {
+		SumConcurrently(input, 4)
+	}
 }
 
 func BenchmarkFiveGoroutines(b *testing.B) {
-	SumConcurrently(input, 5)
+	for i := 0; i < b.N; i++ {
+		SumConcurrently(input, 5)
+	}
+}
+
+func BenchmarkTenGoroutines(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumConcurrently(input, 10)
+	}
 }
