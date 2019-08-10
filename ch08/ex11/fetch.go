7ad24bede82fe4add52ff2064761f1bb1661b76d
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-08-10 12:16:48 +0200

    Exercise 8.11: concurrent fetch for fastest host with cancellation

---
+++ ch08/ex11/fetch.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var cancel = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-cancel:
+		return true
+	default:
+		return false
+	}
+}
+
+func main() {
+	flag.Parse()
+	urls := flag.Args()
+	responses := make(chan string, len(urls))
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		go func(url string) {
+			defer wg.Done()
+			wg.Add(1)
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "build request to %s: %v\n", url, err)
+				return
+			}
+			req.Cancel = cancel
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			defer resp.Body.Close()
+			responses <- url
+			fmt.Println("finished lookup of", url)
+		}(url)
+	}
+	for {
+		select {
+		case url := <-responses:
+			fmt.Println(url, "is the fastest")
+			close(cancel)
+			wg.Wait()
+			return
+		}
+	}
+}
