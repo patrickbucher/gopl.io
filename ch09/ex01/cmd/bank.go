06731661158cead10156d7f667baeea4f06feb43
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-08-11 15:35:10 +0200

    Exercise 9.1: fixed wait group (add before goroutine)

--- ch09/ex01/cmd/bank.go
+++ ch09/ex01/cmd/bank.go
@@ -9,25 +9,27 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	fmt.Println(bank.Balance())
 
 	// Alice:
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		wg.Add(1)
 		bank.Withdraw(500)
 		bank.Deposit(1000)
 		bank.Deposit(100)
 	}()
 
 	// Bob:
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		wg.Add(1)
 		bank.Deposit(100)
 		bank.Deposit(200)
 		bank.Withdraw(1000)
 		bank.Withdraw(1000)
 	}()
+
 	wg.Wait()
 	fmt.Println(bank.Balance())
 }
